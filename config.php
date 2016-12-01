d8d85d13ede76d2453ddc7e33e509fdfc14617cf
Etienne <Etienne.JODRY@hotmail.fr>
2016-12-01 20:31:25 +0100

    config

--- config.php
+++ config.php
@@ -1,1 +1,12 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
 
+function loadClass($class) {
+		$dir = "model";
+		if (strpos($class,"Controller") !== FALSE)
+				$dir = "controller";
+		include_once "$dir/$class.php";
+}
+
+spl_autoload_register('loadClass');
