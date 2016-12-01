79f51f15e58c92e16cdc0ef74b89f5b98b2a6a4e
tekreme73 <tekreme73@gmail.com>
2016-12-01 22:43:32 +0100

    [FIX] MySQL script to create DB for the project !

--- script_dahunicorn.sql
+++ script_dahunicorn.sql
@@ -1,76 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: 127.0.0.1
+-- Généré le: Jeu 01 Décembre 2016 à 21:41
+-- Version du serveur: 5.5.53-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-drop table if exists lieu cascade;
-drop table if exists type cascade;
-drop table if exists typelieu cascade;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Base de données: `c9`
+--
 
-/*==============================================================*/
-/* Table : lieu                                    */
-/*==============================================================*/
-create table lieu (
-   idlieu             		MEDIUMINT               not null AUTO_INCREMENT,
-   lieu_nom              	VARCHAR(50)          null,
-   lieu_description         VARCHAR(200)          null,
-   lieu_periode          	VARCHAR(50)          null,
-   lieu_rue        			VARCHAR(50)         null,
-   lieu_complement        	VARCHAR(50)         null,
-   lieu_cp               	VARCHAR(10)          null,
-   lieu_ville            	VARCHAR(50)          null,
-   lieu_latitude            NUMERIC(8)          null,
-   lieu_longitude           NUMERIC(8)                 null,
-   constraint PK_RSR primary key (idlieu)
-);
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `lieu`
+--
 
+CREATE TABLE IF NOT EXISTS `lieu` (
+  `idlieu` int(11) NOT NULL AUTO_INCREMENT,
+  `lieu_nom` varchar(50) DEFAULT NULL,
+  `lieu_description` varchar(200) DEFAULT NULL,
+  `lieu_periode` varchar(50) DEFAULT NULL,
+  `lieu_rue` varchar(50) DEFAULT NULL,
+  `lieu_complement` varchar(50) DEFAULT NULL,
+  `lieu_cp` varchar(10) DEFAULT NULL,
+  `lieu_ville` varchar(50) DEFAULT NULL,
+  `lieu_latitude` decimal(8,0) DEFAULT NULL,
+  `lieu_longitude` decimal(8,0) DEFAULT NULL,
+  PRIMARY KEY (`idlieu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
-/*==============================================================*/
-/* Table : type                                    */
-/*==============================================================*/
-create table type (
-   idtype             		MEDIUMINT               not null AUTO_INCREMENT,
-   type_libelle             VARCHAR(50)          null,
-   constraint PK_RSR primary key (idtype)
-);
+--
+-- Structure de la table `type`
+--
 
+CREATE TABLE IF NOT EXISTS `type` (
+  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `type_libelle` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idtype`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `typelieu`
+--
 
-/*==============================================================*/
-/* Table : typelieu                                   */
-/*==============================================================*/
-create table typelieu (
-   idtype             		    INT(4)          null,
-   idlieu             		    INT(4)          null,
-   typelieu_quantite		    INT(4)		  null,
-   typelieu_quantite_estimee  INT(4)       null,
-   constraint PK_RSR primary key (idtype, idlieu)
-);
+CREATE TABLE IF NOT EXISTS `typelieu` (
+  `idtype` int(4) NOT NULL DEFAULT '0',
+  `idlieu` int(4) NOT NULL DEFAULT '0',
+  `typelieu_quantite` int(4) DEFAULT NULL,
+  `typelieu_quantite_estimee` int(4) DEFAULT NULL,
+  PRIMARY KEY (`idtype`,`idlieu`),
+  KEY `idlieu` (`idlieu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- RELATIONS POUR LA TABLE `typelieu`:
+--   `idlieu`
+--       `lieu` -> `idlieu`
+--   `idtype`
+--       `type` -> `idtype`
+--
 
+--
+-- Contraintes pour les tables exportées
+--
 
+--
+-- Contraintes pour la table `typelieu`
+--
+ALTER TABLE `typelieu`
+  ADD CONSTRAINT `typelieu_ibfk_2` FOREIGN KEY (`idlieu`) REFERENCES `lieu` (`idlieu`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `typelieu_ibfk_1` FOREIGN KEY (`idtype`) REFERENCES `type` (`idtype`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
-
-
-
-alter table typelieu
-   add constraint FK_TYPSIT_TYP foreign key (idtype)
-      references type (idtype)
-      on delete restrict on update restrict;
-create index IX_TYPSIT_TYP_ID ON typelieu(idtype);
-
-
-
-
-alter table typelieu
-   add constraint FK_TYPSIT_SIT foreign key (idlieu)
-      references lieu (idlieu)
-      on delete restrict on update restrict;
-create index IX_TYPSIT_SIT_ID ON typelieu(idlieu);
-
-
-
-
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
