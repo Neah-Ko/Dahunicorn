c2138bbd4775a3c57dcc65c31e5f06dac28e1910
Etienne <Etienne.JODRY@hotmail.fr>
2016-12-01 21:44:10 +0100

    Classes

--- model/Association.php
+++ model/Association.php
@@ -1,4 +1,9 @@
 <?php
 class Association extends Model() {
-	
+	protected $_id_association;
+	protected $_nom;
+	protected $_description;
+	protected $_aso_id;
+	protected $_aso_id;
+	protected $_aso_id;
 }
