78d34f7d088901256f1b4e4eef5d95f8a5e811ca
tekreme73 <tekreme73@gmail.com>
2016-12-02 01:31:58 +0100

    [ADD] HTML import to display Google Maps

--- public/view/header.php
+++ public/view/header.php
@@ -5,7 +5,14 @@
 	<meta charset="UTF-8" />
 	<title>InfoMigrant</title>
 	<link rel="stylesheet" href="styles/style.css" />
+
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js"></script>
+	<script title="webcomponentsjs" src="/bower/webcomponentsjs/webcomponents-lite.min.js"></script>
+
+	<link rel="import" href="/bower/google-map/google-map.html">
+	<link rel="import" href="/bower/google-map/google-map-marker.html">
+	<link rel="import" href="/bower/google-map/google-map-poly.html">
+	<link rel="import" href="/bower/google-map/google-map-directions.html">
 </head>
 
 <body>
