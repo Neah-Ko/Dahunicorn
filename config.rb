3b62657a389298f64a31ade1a56fc65d1437f0b7
Vincent PEILLEX <peillex.vincent@live.fr>
2016-12-02 03:05:33 +0100

    Modification répertoire SASS

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 preferred_syntax = :scss
 http_path = '/'
 css_dir = 'public/view/styles'
-sass_dir = 'ressources/sass'
+sass_dir = 'public/resources/sass'
 images_dir = 'public/view/images'
 javascripts_dir = 'public/js'
 relative_assets = true
