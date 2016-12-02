6d7a2a81af06804d1fdf98475b049fb18fb7a142
tekreme73 <tekreme73@gmail.com>
2016-12-02 01:42:24 +0100

    [UPDATE] Google Maps display

--- public/view/header.php
+++ public/view/header.php
@@ -4,15 +4,16 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>InfoMigrant</title>
+	<base href="/public"/>
 	<link rel="stylesheet" href="styles/style.css" />
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js"></script>
-	<script title="webcomponentsjs" src="/bower/webcomponentsjs/webcomponents-lite.min.js"></script>
+	<script title="webcomponentsjs" src="/public/bower/webcomponentsjs/webcomponents-lite.min.js"></script>
 
-	<link rel="import" href="/bower/google-map/google-map.html">
-	<link rel="import" href="/bower/google-map/google-map-marker.html">
-	<link rel="import" href="/bower/google-map/google-map-poly.html">
-	<link rel="import" href="/bower/google-map/google-map-directions.html">
+	<link rel="import" href="/public/bower/google-map/google-map.html">
+	<link rel="import" href="/public/bower/google-map/google-map-marker.html">
+	<link rel="import" href="/public/bower/google-map/google-map-poly.html">
+	<link rel="import" href="/public/bower/google-map/google-map-directions.html">
 </head>
 
 <body>
--- public/view/site/index.php
+++ public/view/site/index.php
@@ -53,9 +53,6 @@ $markers = [
 
 ?>
 
-
-<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyCw3j24INjRLtWf3fAlKnWC4GtksBIMiHs'></script><div style='overflow:hidden;height:400px;width:520px;'><div id='gmap_canvas' style='height:400px;width:520px;'></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div> <a href='https://add-map.org/'>google maps embedded</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=aff112c57315aa96ee4c4536fd6cd844e6ac64d5'></script><script type='text/javascript'>function init_map(){var myOptions = {zoom:12,center:new google.maps.LatLng(45.5646,5.926500000000033),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(45.5646,5.926500000000033)});infowindow = new google.maps.InfoWindow({content:'<strong></strong><br><br> Chambéry<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
-
 <section class="">
   <div class="maps">
     <google-map class="maps-cmp" language="fr" min-zoom="7" latitude="45.525241" longitude="5.84505" fit-to-markers api-key="AIzaSyBTswMklmPyz-JLcu_L8gO2T_8pmucovM0">
