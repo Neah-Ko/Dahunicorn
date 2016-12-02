64bf815fa969795c640abc61e4efe819b09c5d5a
Vincent PEILLEX <peillex.vincent@live.fr>
2016-12-02 01:07:46 +0100

    Modification s mineures

--- public/view/lieu/fiche.php
+++ public/view/lieu/fiche.php
@@ -1,23 +1,10 @@
-
 <div>
-	
 	<h1>Ce lieu</h1>
-
 	<form type="POST" action="?r=lieu/addReserve">
-
 		<label for="nbpersonnes">Nombre de personnes :</label>
 		<input type="text" name="nbpersonnes">
-
 		<input type="submit" name="reserver" value="reserver">
-
 	</form>
-	
-
-
-
-
-
-
 </div>
 
 
